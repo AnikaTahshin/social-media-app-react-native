4deaf850662494b228964461e921be0a42354e61
unknown <anikatahsinmeem@gmail.com>
2023-12-13 22:46:05 +0600

    login page done

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,14 +1,104 @@
-import { KeyboardAvoidingView, StyleSheet, Text, View } from "react-native";
-import React from "react";
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React, { useState } from "react";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
 
 const LoginScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <KeyboardAvoidingView style={styles.container} behavior="padding">
-      <Text>LoginScreen</Text>
-    </KeyboardAvoidingView>
+    <SafeAreaView style={styles.container}>
+      <StatusBar animated={true} backgroundColor="#61dafb" />
+
+      {/* FOR EMAIL AND PASSWORD TO LOGIN  */}
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Email"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+          style={styles.input}
+        />
+
+        <TextInput
+          placeholder="Password"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+          style={styles.input}
+          secureTextEntry
+        />
+      </View>
+
+      {/* LOGIN AND REGISTER BUTTON  */}
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          // onPress={}
+          style={[styles.button]}
+        >
+          <Text style={styles.buttonTxt}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          // onPress={}
+          style={[styles.button, styles.buttonOutline]}
+        >
+          <Text style={styles.buttonOutlineTxt}>Register</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 };
 
 export default LoginScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    width: wp("100%"),
+  },
+
+  inputContainer: {
+    width: wp("80%"),
+  },
+  input: {
+    backgroundColor: "white",
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 10,
+    marginTop: 5,
+  },
+  buttonContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 40,
+  },
+
+  button: {
+    backgroundColor: "blue",
+    width: wp("60%"),
+    padding: 15,
+    borderRadius: 10,
+  },
+  buttonOutline: {
+    backgroundColor: "white",
+    marginTop: 10,
+    borderColor: "blue",
+    borderWidth: 2,
+  },
+  buttonTxt: { textAlign: "center", color: "white", fontSize: hp(2) },
+  buttonOutlineTxt: {
+    textAlign: "center",
+    fontSize: hp(2),
+  },
+});
